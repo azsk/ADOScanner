9988758646ca80f870e6b7d246712327d284a68c
v-arbagh@microsoft.com <v-arbagh@microsoft.com>
2021-04-09 16:40:25 +0530

    message changes

--- src/AzSK.ADO/Framework/BugLog/BugLogHelper.ps1
+++ src/AzSK.ADO/Framework/BugLog/BugLogHelper.ps1
@@ -120,7 +120,7 @@ class BugLogHelper {
         catch
         {
             #Write-Host $_
-            Write-Host "Could not fetch the entry for partition key [$hash] in the table storage or table is not found.";
+            Write-Host "Could not fetch the entry for partition key [$hash] in the table storage or the table was not found.";
             return $null
         }
     }
