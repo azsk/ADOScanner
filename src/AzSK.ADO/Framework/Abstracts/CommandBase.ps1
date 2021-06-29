1dbe1a571dc793aa148c8c958c74a77a92ad0573
t-rbavdekar <t-rbavdekar@microsoft.com>
2021-06-29 12:37:33 +0530

    modified:   src/AzSK.ADO/Framework/Abstracts/CommandBase.ps1

--- src/AzSK.ADO/Framework/Abstracts/CommandBase.ps1
+++ src/AzSK.ADO/Framework/Abstracts/CommandBase.ps1
@@ -182,7 +182,9 @@ class CommandBase: AzSKRoot {
 				    $sarifMethodResults=[PartialScanManager]::ControlResultsWithSARIFSummary
                 }
 			}
-		    [SARIFLogsGenerator]::new($sarifMethodResults,$folderPath,$this.RunIdentifier)
+			if($sarifMethodResults){
+		    	[SARIFLogsGenerator]::new($sarifMethodResults,$folderPath,$this.RunIdentifier)
+			}
 			[PartialScanManager]::ControlResultsWithSARIFSummary=@()
         }
 		# Publish command complete events
