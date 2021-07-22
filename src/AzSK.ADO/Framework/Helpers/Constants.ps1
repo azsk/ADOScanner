d524854b3c107cec3929c059af0ae30fc623a7ea
Sree Bhattacharyya <t-sreeb@microsoft.com>
2021-07-22 13:53:40 +0530

    timestamps file added to constants

--- src/AzSK.ADO/Framework/Helpers/Constants.ps1
+++ src/AzSK.ADO/Framework/Helpers/Constants.ps1
@@ -87,6 +87,7 @@ class Constants
 	static [string] $CAScanLogsContainerName = "ado-scan-logs"
 	static [string] $ResourceScanTrackerBlobName = "ResourceScanTracker.json"
 	static [string] $ResourceScanTrackerCMBlobName = "ResourceScanTracker_CentralMode.json"
+	static [string] $IncrementalScanTimeStampFile = "IncrementalScanTimestamp.json"
 	static [hashtable] $AttestationStatusHashMap = @{
 			[AttestationStatus]::NotAnIssue		="1";
 			[AttestationStatus]::WillNotFix		="2";
