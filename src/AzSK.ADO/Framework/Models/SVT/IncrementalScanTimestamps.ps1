311ddce002461a915f074b63c87b99b88bd8203c
Sree Bhattacharyya <t-sreeb@microsoft.com>
2021-07-26 16:28:55 +0530

    removed org name from class

--- src/AzSK.ADO/Framework/Models/SVT/IncrementalScanTimestamps.ps1
+++ src/AzSK.ADO/Framework/Models/SVT/IncrementalScanTimestamps.ps1
@@ -1,15 +1,9 @@
 Set-StrictMode -Version Latest
 class IncrementalScanTimestamps
 {
-    [string] $orgName = 0
 	[datetime] $Build = 0
     [datetime] $Release = 0
     [datetime] $AgentPools = 0
     [datetime] $VariableGroups = 0
     [datetime] $SecureFiles = 0
-
-    IncrementalScanTimestamps([string] $OrganizationName)
-    {
-        $this.orgName = $OrganizationName
-    }
 }
