7da7dd2772f0fc99097ce6ba2bea3883f6cd2535
juhi037 <juhitiwari@microsoft.com>
2022-01-12 15:06:11 +0530

    bug template mandatory

--- src/AzSK.ADO/AutoBugLogCMD/AutoBugLogSVT.ps1
+++ src/AzSK.ADO/AutoBugLogCMD/AutoBugLogSVT.ps1
@@ -221,6 +221,9 @@ function Start-AzSKADOBugLogging
 			}
 
 			$isValidBugTemplate = $false;
+			if($AutoBugLog -and [string]::IsNullOrWhiteSpace($BugTemplateFilePath)){
+				Write-Host 'Bug template file path is mandatory with AutoBugLog. Please provide correct value in [BugTemplateFilePath] command parameter.' -ForegroundColor Red;
+			}
 			if(![string]::IsNullOrWhiteSpace($BugTemplateFilePath) -and (Test-Path $BugTemplateFilePath)) {
 				Write-Host 'Validating bug template.....' -ForegroundColor Cyan
 				$BugTemplate = Get-content $BugTemplateFilePath | ConvertFrom-Json;
